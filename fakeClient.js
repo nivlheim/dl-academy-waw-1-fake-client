e745c34cee11169760941043f6a1313c6c1b3518
Swacha Rafał <Rafal.Swacha@dreamlab.pl>
2018-04-08 13:55:20 +0200

    message handling structure

--- fakeClient.js
+++ fakeClient.js
@@ -1,5 +1,16 @@
+const _ = require('lodash');
+
 const ClientHandler = require('./lib/ClientHandler');
 const fileReader = require('./lib/fileReader');
 
-const clients = fileReader.read('clients.txt');
+const clients = [];
+
+// Read clients.txt file, create new client handler and push it to the array.
+_.forEach(fileReader.read('clients.txt'), clientAddress => {
+	clients.push(new ClientHandler(clientAddress));
+});
+
+// Run all clients.
+_.forEach(clients, client => client.run());
+
 
