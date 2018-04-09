8720f4e91e43d66f6207088dca2ad59d86b42027
Swacha Rafał <Rafal.Swacha@dreamlab.pl>
2018-04-09 13:54:34 +0200

    requests

--- fakeClient.js
+++ fakeClient.js
@@ -3,11 +3,14 @@ const _ = require('lodash');
 const ClientHandler = require('./lib/ClientHandler');
 const fileReader = require('./lib/fileReader');
 
+const CLIENTS_PER_SERVER = 3;
 const clients = [];
 
 // Read clients.txt file, create new client handler and push it to the array.
 _.forEach(fileReader.read('clients.txt'), clientAddress => {
-	clients.push(new ClientHandler(clientAddress));
+	for (let i = 0; i < CLIENTS_PER_SERVER; i++) {
+		clients.push(new ClientHandler(clientAddress));
+	}
 });
 
 // Run all clients.
