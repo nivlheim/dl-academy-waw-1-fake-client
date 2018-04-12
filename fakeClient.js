96aa69151940c9d44fc3dfbd8cd58c5fcf2d1f16
Swacha Rafał <Rafal.Swacha@dreamlab.pl>
2018-04-12 12:14:02 +0200

    Code cleanup.

--- fakeClient.js
+++ fakeClient.js
@@ -1,23 +1,25 @@
 const _ = require('lodash');
 
 const ClientHandler = require('./lib/ClientHandler');
-const FileReader = require('./lib/FileReader');
+const readFileToArray = require('./lib/readFileToArray');
 
 const CLIENTS_PER_SERVER = 3;
+const servers = readFileToArray('servers.txt');
 const clients = [];
 
-// Read clients.txt file, create new client handler and push it to the array.
-_.forEach(FileReader.read('servers.txt'), serverAddress => {
+// Create new client handler and push it to the array.
+servers.forEach(serverAddress => {
 	for (let i = 0; i < CLIENTS_PER_SERVER; i++) {
 		clients.push(new ClientHandler(serverAddress));
 	}
 });
 
 // Run all clients.
-_.forEach(clients, client => client.run());
+clients.forEach(client => client.run());
 
 // Catch application stopping signal, disconnect clients and quit gracefully.
 process.on('SIGINT', () => {
-	return Promise.all(_.map(clients, client => client.disconnect()))
+	const disconnectPromises = _.map(clients, client => client.disconnect());
+	return Promise.all(disconnectPromises)
 		.then(() => process.exit);
 });
